d153b758b139cbaf8a8b975d62dc9e4dc037d3d0
Pascal <pascal@pascal.com>
2023-12-04 12:20:58 -0500

    Initial reveal.js scaffolding

---
+++ src/lib/deck/Slides.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	import Reveal from 'reveal.js';
+	import Markdown from 'reveal.js/plugin/markdown/markdown';
+	import Highlight from 'reveal.js/plugin/highlight/highlight';
+	import Notes from 'reveal.js/plugin/notes/notes';
+	import 'reveal.js/dist/reveal.css';
+	import 'reveal.js/dist/theme/black.css';
+	import 'reveal.js/plugin/highlight/monokai.css';
+
+	onMount(() => {
+		const deck = new Reveal({
+			autoAnimateEasing: 'ease',
+			autoAnimateDuration: 1,
+			hash: true,
+			controls: true,
+			progress: true
+		});
+
+		deck.initialize({
+			controls: true,
+			progress: true,
+			center: true,
+			hash: true,
+			slideNumber: true,
+			transition: 'slide',
+			plugins: [Markdown, Highlight, Notes]
+		});
+	});
+</script>
+
+<div class="reveal">
+	<div class="slides">
+		<section>slide 1</section>
+		<section>slide 2</section>
+	</div>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import '../app.postcss';
+
+	import Slides from '$lib/deck/Slides.svelte';
+</script>
+
+<svelte:head>
+	<title>Slides</title>
+</svelte:head>
+
+<Slides />
