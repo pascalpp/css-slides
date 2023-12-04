ed72f9a4eddb545dccec16ccfd297166c99aabef
Pascal <pascal@pascal.com>
2023-12-04 16:25:44 -0500

    ugly/beautiful cross fade idea

--- src/lib/deck/TheGoodTheBadTheBeautiful.svelte
+++ src/lib/deck/TheGoodTheBadTheBeautiful.svelte
@@ -2,38 +2,53 @@
   import Slide from './Slide.svelte';
 </script>
 
-<Slide>
+<section data-transition="fade">
+  <div class="box">
+    <h1>CSS</h1>
+    <p>
+      The Good, The Bad, The&nbsp;<span class="relative"><span class="ugly">Ugly</span></span>
+    </p>
+  </div>
+</section>
+
+<section data-transition="fade">
   <div class="box">
     <h1>CSS</h1>
     <p>
       The Good, The Bad, The <span class="relative">
-        <span class="ugly">Ugly</span><span class="beautiful">Beautiful</span>
+        <span class="ugly crossed-out">Ugly</span><span class="beautiful">Beautiful</span>
       </span>
     </p>
   </div>
-</Slide>
+</section>
 
-<style lang="postcss">
+<style lang="less">
+  .box {
+    transform: scale(1.5);
+  }
+
   .relative {
     position: relative;
   }
 
   .ugly {
     position: absolute;
-    bottom: 85%;
-    left: -5px;
-    transform: rotate(-10deg);
-    color: hsl(0, 100%, 70%);
-    margin: 0;
-    padding-inline: 8px;
-    &::after {
-      content: '';
-      position: absolute;
-      top: 50%;
-      left: 0;
-      width: 100%;
-      height: 4px;
-      background: hsl(0, 100%, 70%);
+    &.crossed-out {
+      bottom: 85%;
+      left: -5px;
+      transform: rotate(-10deg);
+      color: hsl(0, 100%, 70%);
+      margin: 0;
+      padding-inline: 8px;
+      &::after {
+        content: '';
+        position: absolute;
+        top: 50%;
+        left: 0;
+        width: 100%;
+        height: 4px;
+        background: hsl(0, 100%, 70%);
+      }
     }
   }
 
