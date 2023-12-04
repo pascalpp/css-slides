dc0bc3d3403e1d09319b3b1283a9f203861f5437
Pascal <pascal@pascal.com>
2023-12-04 17:05:06 -0500

    remove transition when going backwards

--- src/lib/deck/TheGoodTheBadTheBeautiful.svelte
+++ src/lib/deck/TheGoodTheBadTheBeautiful.svelte
@@ -25,21 +25,21 @@
 
   .ugly {
     position: absolute;
-    transition: all var(--transition-time) ease-in-out var(--transition-time);
     &::after {
       content: '';
       position: absolute;
-      transition: all var(--transition-time) ease-in-out var(--transition-time);
     }
   }
 
   .box:has(.beautiful.visible) {
     .ugly {
+      transition: all var(--transition-time) ease-in-out var(--transition-time);
       transform: translateY(-90%) rotate(-10deg);
       color: hsl(0, 100%, 70%);
       margin: 0;
       padding-inline: 8px;
       &::after {
+        transition: all var(--transition-time) ease-in-out var(--transition-time);
         content: '';
         position: absolute;
         top: 50%;
@@ -57,10 +57,10 @@
     color: var(--secondary-color);
     max-width: 0;
     opacity: 0;
-    transition:
-      max-width var(--transition-time) ease-in-out 0s,
-      opacity var(--transition-time) ease-in-out calc(var(--transition-time) * 2);
     &:is(.visible) {
+      transition:
+        max-width var(--transition-time) ease-in-out 0s,
+        opacity var(--transition-time) ease-in-out calc(var(--transition-time) * 2);
       max-width: 200px;
       opacity: 1;
     }
