83e46b513899ed9395f57694537eddd56a9410d3
Pascal <pascal@pascal.com>
2023-12-04 18:43:53 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import svelteSVG from 'vite-plugin-svelte-svg'; // import the plugin
+import svelteSVG from 'vite-plugin-svelte-svg';
 
 export default defineConfig({
-  plugins: [sveltekit(), svelteSVG()],
+  plugins: [
+    sveltekit(),
+    svelteSVG({
+      svgoConfig: {},
+      requireSuffix: false,
+    }),
+  ],
   define: {
     'process.env': {},
   },
