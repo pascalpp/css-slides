34eba413879fcb446b62cefd2d79400d49c118ce
Pascal <pascal@pascal.com>
2023-12-04 16:26:12 -0500

    add container queries section

--- src/lib/deck/Presentation.svelte
+++ src/lib/deck/Presentation.svelte
@@ -1,31 +1,46 @@
 <script lang="ts">
-	import Code from './Code.svelte';
-	import Slide from './Slide.svelte';
-	import snippet from './Code.svelte?raw';
+  import Code from './Code.svelte';
+  import Slide from './Slide.svelte';
+  import snippet from './Code.svelte?raw';
 
-	import TheGoodTheBadTheBeautiful from './TheGoodTheBadTheBeautiful.svelte';
-	import HowToLiveLaughLove from './HowToLiveLaughLove.svelte';
+  import TheGoodTheBadTheBeautiful from './TheGoodTheBadTheBeautiful.svelte';
+  import HowToLiveLaughLove from './HowToLiveLaughLove.svelte';
 </script>
 
 <TheGoodTheBadTheBeautiful />
 <HowToLiveLaughLove />
 
-<Slide animate>
-	<Slide animate>
-		<h1>Animate</h1>
-	</Slide>
+<section>
+  <h1>Container Queries</h1>
+  <ul class="fragment">
+    <li>Like media queries</li>
+    <li class="fragment">But viewport-agnostic</li>
+    <li class="fragment">And element-specific</li>
+    <li class="fragment">
+      Stop asking <span class="fragment strike highlight-red semi-fade-out">“How big is the screen?”</span>
+    </li>
+    <li class="fragment">
+      Start asking <span class="fragment grow lime">“How big am I?”</span>
+    </li>
+  </ul>
+</section>
 
-	<Slide animate>
-		<Code>{snippet}</Code>
-	</Slide>
+<Slide animate>
+  <Slide animate>
+    <h1>Animate</h1>
+  </Slide>
+
+  <Slide animate>
+    <Code>{snippet}</Code>
+  </Slide>
 </Slide>
 
 <Slide animate>
-	<Slide animate>
-		<h1>Animate</h1>
-	</Slide>
+  <Slide animate>
+    <h1>Animate</h1>
+  </Slide>
 
-	<Slide animate>
-		<Code>{snippet}</Code>
-	</Slide>
+  <Slide animate>
+    <Code>{snippet}</Code>
+  </Slide>
 </Slide>
