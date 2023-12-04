f94ab5a39390e3420174197355647b553440a4c0
Pascal <pascal@pascal.com>
2023-12-04 16:00:42 -0500

    Update TheGoodTheBadTheBeautiful.svelte

--- src/lib/deck/TheGoodTheBadTheBeautiful.svelte
+++ src/lib/deck/TheGoodTheBadTheBeautiful.svelte
@@ -2,18 +2,44 @@
   import Slide from './Slide.svelte';
 </script>
 
-<Slide animate>
-  <h1>CSS</h1>
-  <h2>
-    The Good, The Bad, The <span class="relative">
-      <strike>Ugly</strike><span class="larger">Beautiful</span>
-    </span>
-  </h2>
+<Slide>
+  <div class="box">
+    <h1>CSS</h1>
+    <p>
+      The Good, The Bad, The <span class="relative">
+        <span class="ugly">Ugly</span><span class="beautiful">Beautiful</span>
+      </span>
+    </p>
+  </div>
 </Slide>
 
 <style lang="postcss">
-  h1 {
-    color: white;
-    margin-bottom: 0;
+  .relative {
+    position: relative;
+  }
+
+  .ugly {
+    position: absolute;
+    bottom: 85%;
+    left: -5px;
+    transform: rotate(-10deg);
+    color: hsl(0, 100%, 70%);
+    margin: 0;
+    padding-inline: 8px;
+    &::after {
+      content: '';
+      position: absolute;
+      top: 50%;
+      left: 0;
+      width: 100%;
+      height: 4px;
+      background: hsl(0, 100%, 70%);
+    }
+  }
+
+  .beautiful {
+    font-family: var(--font-serif);
+    font-style: italic;
+    color: var(--secondary-color);
   }
 </style>
