a230b07cb7b6ec1ac07b791bb7916f07e24a86ed
Pascal <pascal@pascal.com>
2023-12-05 13:15:53 -0500

    Create FlexboxGrowShrink.svelte

---
+++ src/lib/deck/FlexboxGrowShrink.svelte
@@ -0,0 +1,210 @@
+<script lang="ts">
+  import Browser from '$lib/components/Browser.svelte';
+  import { getContext, onMount } from 'svelte';
+  import Code from './Code.svelte';
+  import type { Api } from 'reveal.js';
+
+  type FragmentEvent = Event & { fragment: HTMLElement };
+
+  let reveal: Api | null = null;
+  const context = getContext('reveal') as Record<string, null | Api>;
+
+  let width = '100%';
+
+  function changeWidth() {
+    width = width === '100%' ? '80%' : '100%';
+  }
+
+  let timer = 0;
+
+  function onFragmentShown(event: Event) {
+    const fragmentEvent = event as FragmentEvent;
+    console.log('fragmentshown', event);
+    if (fragmentEvent.fragment?.classList.contains('grow-note')) {
+      timer = setInterval(changeWidth, 1500);
+    } else {
+      clearInterval(timer);
+      width = '100%';
+    }
+  }
+
+  onMount(() => {
+    setTimeout(() => {
+      reveal = context?.reveal;
+      reveal?.on('fragmentshown', onFragmentShown);
+    }, 1000);
+  });
+  // $: reveal = getContext('reveal');
+</script>
+
+<section data-transition="zoom-in slide-out">
+  <Browser title="Flexbox: grow and shrink" {width} on:click={changeWidth}>
+    <div class="page">
+      <h2>flex-grow & flex-shrink</h2>
+      <div class="box panels fragment" data-fragment-index="1">
+        <div class="pane left fragment" data-fragment-index="3">
+          <div class="label">Sidebar</div>
+          <div class="fragment pane-style-1" data-fragment-index="5">
+            <Code>{`width: 300px;`}</Code>
+          </div>
+          <div class="fragment pane-style-2" data-fragment-index="60">
+            <Code>
+              {`
+                width: 300px;
+                flex-shrink: 0;
+              `}
+            </Code>
+          </div>
+        </div>
+        <div class="pane right fragment" data-fragment-index="4">
+          <div class="label">Detail</div>
+          <div class="fragment" data-fragment-index="6">
+            <Code>{`flex-grow: 1;`}</Code>
+            <div class="box wrapper fragment grow-note" data-fragment-index="7">
+              <p>Detail pane consumes any available space, even as viewport changes size.</p>
+            </div>
+            <div class="fragment wide-content" data-fragment-index="8">
+              <p>Suppose some “wide content” is added, squeezing our sidebar.</p>
+              <p class="fragment" data-fragment-index="9">We can apply flex-shrink to fix this.</p>
+              <p class="fragment" data-fragment-index="61">Fixed!</p>
+            </div>
+          </div>
+        </div>
+        <div class="fragment flex-row-code" data-fragment-index="2">
+          <Code>
+            {`
+            display: flex;
+            flex-direction: row;
+            `}
+          </Code>
+        </div>
+      </div>
+    </div>
+  </Browser>
+</section>
+
+<style lang="less">
+  .page {
+    padding: 1rem;
+    height: 55vh;
+    position: relative;
+    h2 {
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      white-space: nowrap;
+      transform: translateX(-50%) translateY(-50%);
+    }
+    &:has(.panels .fragment.visible) {
+      h2 {
+        display: none;
+      }
+    }
+  }
+
+  .panels {
+    display: flex;
+    flex-direction: row;
+    align-items: stretch;
+    gap: 1rem;
+    padding: 0;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+
+    .flex-row-code {
+      padding-inline: 0.5em;
+    }
+
+    &:has(.pane.right .fragment.visible) {
+      .flex-row-code {
+        display: none;
+      }
+    }
+  }
+
+  .pane {
+    background-color: var(--color-translucent-lime);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-top: 5rem;
+    position: relative;
+    transition: all 0.5s ease-in-out;
+
+    .label {
+      position: absolute;
+      top: 0;
+      left: 0;
+      padding-block: 0.5rem;
+      padding-inline: 1rem;
+    }
+  }
+  .pane.left {
+    display: none;
+    &:is(.visible) {
+      display: flex;
+    }
+
+    &:has(.fragment.visible) {
+      width: 300px;
+    }
+    .pane-style-2 {
+      display: none;
+      &:is(.visible) {
+        display: block;
+      }
+    }
+
+    &:has(.fragment.pane-style-2.visible) {
+      flex-shrink: 0;
+      .pane-style-1 {
+        display: none;
+      }
+    }
+  }
+  .pane.right {
+    display: none;
+    &:is(.visible) {
+      display: flex;
+    }
+    &:has(.fragment.visible) {
+      flex-grow: 1;
+    }
+
+    .grow-note {
+      max-width: 370px;
+      padding: 0.5em 1em;
+      margin: 0 auto;
+      margin-top: 2rem;
+      font-size: 0.8em;
+      align-self: center;
+      display: none;
+      &:is(.visible) {
+        display: block;
+      }
+    }
+
+    &:has(.fragment.wide-content.visible) {
+      .grow-note {
+        display: none;
+      }
+    }
+
+    .wide-content {
+      max-height: 0;
+      max-width: 0;
+      padding: 1em;
+      overflow: hidden;
+      transition: all 1s ease-in-out;
+      &:is(.visible) {
+        max-width: 100vw;
+        max-height: 100vh;
+      }
+
+      p + p {
+        margin-top: 1rem;
+      }
+    }
+  }
+</style>
