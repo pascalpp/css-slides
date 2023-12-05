a5fc4d82c5a170f69128c3261f2839bbd999f44f
Pascal <pascal@pascal.com>
2023-12-04 23:07:10 -0500

    got container queries slide working with fragments

--- src/lib/deck/ContainerQueries.svelte
+++ src/lib/deck/ContainerQueries.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import Browser from '$lib/components/Browser.svelte';
+  import Code from './Code.svelte';
 
   let narrowBrowser = false;
   let sidebarBrowser = false;
@@ -30,10 +31,16 @@
     </ul>
   </section>
   <section data-transition="fade">
-    <div class="browser-container" class:narrowBrowser class:sidebarBrowser>
+    <div class="browser-container">
       <Browser title="Container queries in action" on:click={handleClick}>
         <div class="page">
-          <h3>&nbsp;</h3>
+          <h3>
+            &nbsp;
+            <span class="fragment wide-browser">Wide browser</span>
+            <span class="fragment narrow-browser">Narrow</span>
+            <span class="fragment sidebar-browser">Sidebar in a wide browser</span>
+            &nbsp;
+          </h3>
           <div class="panels">
             <div class="sidebar">
               <div class="container">
@@ -45,38 +52,116 @@
               </div>
             </div>
             <div class="detail">
-              <div class="card lion"></div>
+              <div class="card lion">
+                <div class="usage">
+                  <div class="fragment custom exclusive">
+                    <h2>Re-use mobile layout</h2>
+                    <p>in any ‘narrow’ context</p>
+                  </div>
+                  <div class="fragment custom exclusive">
+                    <h2>Usage</h2>
+                    <p>1. Wrap your node in a container</p>
+                    <Code lines="1,3"
+                      >{`<div class="container">
+  <div class="cards">…</div>
+</div>`}</Code>
+                    <p class="small">Or use the :host element of your Angular component*</p>
+                  </div>
+                  <div class="fragment custom exclusive">
+                    <h2>Usage</h2>
+                    <p>2. Apply container styles</p>
+                    <Code id="container-css" lines="2-3"
+                      >{`.container {
+  container-name: kittens;
+  container-type: inline-size;
+}`}</Code>
+                    <p class="fragment small">Use <code>inline-size</code> to query width</p>
+                  </div>
+                  <div class="fragment custom exclusive">
+                    <h2>Usage</h2>
+                    <p>3. Apply container query</p>
+                    <Code id="container-css" lines="6-10"
+                      >{`.container {
+  container-name: kittens;
+  container-type: inline-size;
+}
+
+@container kittens (inline-size <= 300px) {
+  .cards {
+    flex-direction: column;
+  }
+}
+`}</Code>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </Browser>
+    </div>
+    <div class="card narrow-notes">
+      <h3>The old way</h3>
+      <Code lines="true"
+        >{`@media screen and (max-width: 640px) {
+  .cards {
+    flex-direction: column;
+  }
+}`}</Code>
     </div>
   </section>
 </section>
 
 <style lang="less">
   .browser-container {
+    margin-top: -1rem;
     max-width: 1000px;
     transition: all 1s ease-in-out;
 
     h3 {
       white-space: nowrap;
+      span {
+        display: inline-block;
+        &:not(.current-fragment) {
+          &:nth-child(1),
+          &:nth-child(2) {
+            display: none;
+          }
+        }
+        &:not(.visible) {
+          display: none;
+        }
+      }
     }
-    h3::before {
-      content: 'Wide browser';
-    }
-    &.narrowBrowser {
+
+    &:has(.fragment.narrow-browser.current-fragment) {
       max-width: calc(300px + 2rem);
 
-      h3::before {
-        content: 'Narrow';
+      + .card.narrow-notes {
+        opacity: 1;
+        transition: opacity 1s ease-in-out 1s;
       }
     }
-    &.sidebarBrowser {
-      h3::before {
-        content: 'Wide browser with sidebar';
+    &:has(.fragment.sidebar-browser.visible) {
+      .sidebar {
+        flex-grow: 0;
+      }
+      .detail {
+        display: block;
       }
     }
+  }
+
+  .card.narrow-notes {
+    width: 550px;
+    height: auto;
+    aspect-ratio: unset;
+    position: absolute;
+    right: 0;
+    top: 50%;
+    transform: translateY(-50%);
+    padding: 2rem;
+    opacity: 0;
   }
 
   .page {
@@ -110,15 +195,6 @@
     }
   }
 
-  .sidebarBrowser {
-    .sidebar {
-      flex-grow: 0;
-    }
-    .detail {
-      display: block;
-    }
-  }
-
   .container {
     container-name: container;
     container-type: inline-size;
@@ -136,6 +212,7 @@
     align-items: center;
     justify-content: center;
     gap: 1rem;
+    padding-bottom: 2rem;
   }
 
   .card {
@@ -159,6 +236,9 @@
       background-image: url('./lion.jpg');
       width: 100%;
       height: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
     }
   }
 
@@ -166,10 +246,28 @@
     .cards {
       flex-direction: column;
       align-items: flex-start;
+      padding-bottom: 0;
     }
     .card {
       width: 100%;
       aspect-ratio: 2/1;
+    }
+  }
+
+  .usage {
+    opacity: 0;
+    width: 100%;
+    height: 100%;
+    background-color: #222;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    padding: 1rem;
+    transition: opacity 0.25s ease-in-out;
+
+    &:has(.fragment.current-fragment) {
+      opacity: 1;
     }
   }
 </style>
