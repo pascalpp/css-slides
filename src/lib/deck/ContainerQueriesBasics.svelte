07254297dc0cda3d25af444da52c90a94b3e8777
Pascal <pascal@pascal.com>
2023-12-06 15:06:57 -0500

    wrap cq basics in a box

--- src/lib/deck/ContainerQueriesBasics.svelte
+++ src/lib/deck/ContainerQueriesBasics.svelte
@@ -1,14 +1,16 @@
 <section>
-  <h1>Container Queries</h1>
-  <ul class="fragment">
-    <li>Like media queries</li>
-    <li class="fragment">But viewport-agnostic</li>
-    <li class="fragment">And element-specific</li>
-    <li class="fragment">
-      Stop asking <span class="fragment strike highlight-red semi-fade-out">“How big is the screen?”</span>
-    </li>
-    <li class="fragment">
-      Start asking <span class="fragment grow lime">“How big am I?”</span>
-    </li>
-  </ul>
+  <div class="box wrapper">
+    <h1>Container Queries</h1>
+    <ul class="fragment">
+      <li>Like media queries</li>
+      <li class="fragment">But viewport-agnostic</li>
+      <li class="fragment">And element-specific</li>
+      <li class="fragment">
+        Stop asking <span class="fragment strike highlight-red semi-fade-out">“How big is the screen?”</span>
+      </li>
+      <li class="fragment">
+        Start asking <span class="fragment grow lime">“How big am I?”</span>
+      </li>
+    </ul>
+  </div>
 </section>
