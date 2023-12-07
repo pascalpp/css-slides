b7387121564fc1abb0689b2f9a8a9f228fdfa5da
Pascal <pascal@pascal.com>
2023-12-07 14:11:02 -0500

    add transition prop

--- src/lib/deck/Slide.svelte
+++ src/lib/deck/Slide.svelte
@@ -2,12 +2,13 @@
   export let id: string | null = null;
   export let animate = false;
   export let restart = false;
+  export let transition = 'slide';
 </script>
 
 <section
+  data-transition={transition}
   data-auto-animate-id={id || null}
   data-auto-animate={animate || null}
-  data-auto-animate-restart={restart || null}
->
+  data-auto-animate-restart={restart || null}>
   <slot />
 </section>
