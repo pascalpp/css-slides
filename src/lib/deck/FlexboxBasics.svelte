4f7b75be4a23b6c1f15d8f671bd8a4a0f7c11842
Pascal <pascal@pascal.com>
2023-12-07 23:06:05 -0500

    Update FlexboxBasics.svelte

--- src/lib/deck/FlexboxBasics.svelte
+++ src/lib/deck/FlexboxBasics.svelte
@@ -1,9 +1,61 @@
 <script lang="ts">
   import Slider from '$lib/components/Slider.svelte';
 
-  let align: 'stretch' | 'flex-start' | 'center' | 'flex-end' = 'stretch';
-  let justify: 'flex-start' | 'center' | 'space-between' | 'flex-end' = 'flex-start';
+  const justifyValues = ['flex-start', 'flex-end', 'center', 'space-between'] as const;
+  type JustifyTuple = typeof justifyValues;
+  type JustifyValue = JustifyTuple[number];
+
+  const alignValues = ['stretch', 'flex-start', 'center', 'flex-end'] as const;
+  type AlignTuple = typeof alignValues;
+  type AlignValue = AlignTuple[number];
+
+  let justify: JustifyValue = 'flex-start';
+  let align: AlignValue = 'stretch';
   let gap = 1;
+
+  let timer = 0;
+
+  function setJustify(value: JustifyValue) {
+    clearInterval(timer);
+    justify = value;
+  }
+
+  function setAlign(value: AlignValue) {
+    clearInterval(timer);
+    align = value;
+  }
+
+  function startJustifyCycle() {
+    clearInterval(timer);
+    showNextJustifyValue();
+    timer = setInterval(showNextJustifyValue, 2000);
+  }
+
+  function startAlignCycle() {
+    clearInterval(timer);
+    showNextAlignValue();
+    timer = setInterval(showNextAlignValue, 2000);
+  }
+
+  function showNextJustifyValue() {
+    const currentIndex = justifyValues.indexOf(justify);
+    const nextIndex = currentIndex + 1;
+    if (nextIndex >= justifyValues.length) {
+      justify = justifyValues[0];
+    } else {
+      justify = justifyValues[nextIndex];
+    }
+  }
+
+  function showNextAlignValue() {
+    const currentIndex = alignValues.indexOf(align);
+    const nextIndex = currentIndex + 1;
+    if (nextIndex >= alignValues.length) {
+      align = alignValues[0];
+    } else {
+      align = alignValues[nextIndex];
+    }
+  }
 </script>
 
 <section>
@@ -16,34 +68,26 @@
             flex-direction: <span class="small">row | column</span>
           </li>
           <li class="fragment justify-content">
-            justify-content:<br />
+            <button on:click={startJustifyCycle}>justify-content:</button><br />
             <span class="indent nowrap small">
-              <button on:click={() => (justify = 'flex-start')} class:active={justify === 'flex-start'}
-                >flex-start</button>
-              |
-              <button on:click={() => (justify = 'flex-end')} class:active={justify === 'flex-end'}>flex-end</button>
-              |
-              <button on:click={() => (justify = 'center')} class:active={justify === 'center'}>center</button>
-              |
-              <button on:click={() => (justify = 'space-between')} class:active={justify === 'space-between'}
-                >space-between</button>
-              | …
+              {#each justifyValues as value}
+                <button on:click={() => setJustify(value)} class:active={justify === value}>{value}</button>
+                |
+              {/each}
+              …
             </span>
           </li>
           <li class="fragment align-items">
-            align-items:<br />
+            <button on:click={startAlignCycle}>align-items:</button><br />
             <span class="indent small">
-              <button on:click={() => (align = 'stretch')} class:active={align === 'stretch'}>stretch</button>
-              |
-              <button on:click={() => (align = 'flex-start')} class:active={align === 'flex-start'}>flex-start</button>
-              |
-              <button on:click={() => (align = 'flex-end')} class:active={align === 'flex-end'}>flex-end</button>
-              |
-              <button on:click={() => (align = 'center')} class:active={align === 'center'}>center</button>
-              | …
+              {#each alignValues as value}
+                <button on:click={() => setAlign(value)} class:active={align === value}>{value}</button>
+                |
+              {/each}
+              …
             </span>
           </li>
-          <li class="fragment">
+          <li class="fragment flex-gap">
             <div class="row gap-row">
               <span>gap: <em class="small">any length</em></span>
               <span class="slider">
@@ -100,13 +144,6 @@
     align-items: flex-end;
   }
 
-  // ul:hover {
-  //   color: #666;
-  //   li:hover {
-  //     color: #fff;
-  //   }
-  // }
-
   ul {
     margin-left: 0;
     font-size: 0.9em;
@@ -135,6 +172,13 @@
     --arrow-color: var(--color-red);
     --arrow-inset: 20px;
     display: none;
+    .label {
+      display: block;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      inset: 0;
+    }
     &.horizontal {
       background-image: linear-gradient(to right, var(--arrow-color) 50%, rgba(255, 255, 255, 0) 0%);
       background-position: left bottom;
@@ -272,9 +316,8 @@
           font-family: var(--font-mono);
           color: var(--color-lime);
           position: absolute;
-          top: 1px;
-          left: 5%;
-          transform: translateY(-50%);
+          top: -2px;
+          transform: translateX(-50%) translateY(-50%);
           padding: 8px;
           border-radius: 4px;
         }
@@ -329,9 +372,9 @@
           font-family: var(--font-mono);
           color: var(--color-lime);
           position: absolute;
-          top: 5%;
+          top: 50%;
           left: 50%;
-          transform: translateX(-50%);
+          transform: translateX(-50%) translateY(-50%);
           padding: 8px;
           border-radius: 4px;
           line-height: 2;
@@ -345,14 +388,21 @@
       .row-example {
         .arrow.horizontal {
           display: block;
+          .label {
+            animation: left-right 5s ease-in-out -5s infinite;
+          }
           .label::before {
             content: 'Content flows along primary axis';
           }
         }
       }
+
       .column-example {
         .arrow.vertical {
           display: block;
+          .label {
+            animation: up-down 5s ease-in-out -5s infinite;
+          }
           .label::before {
             content: 'Content flows along primary axis';
           }
@@ -388,6 +438,30 @@
         .arrow.vertical {
           display: block;
         }
+      }
+    }
+
+    @keyframes left-right {
+      0% {
+        transform: translateX(-12%);
+      }
+      50% {
+        transform: translateX(12%);
+      }
+      100% {
+        transform: translateX(-12%);
+      }
+    }
+
+    @keyframes up-down {
+      0% {
+        transform: translateY(-18%);
+      }
+      50% {
+        transform: translateY(18%);
+      }
+      100% {
+        transform: translateY(-18%);
       }
     }
   }
