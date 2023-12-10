2d4b2a5e93185f4592eec2fdba1155838d10908d
Pascal <pascal@pascal.com>
2023-12-10 12:47:46 -0500

    add swatchgrid

---
+++ src/lib/deck/Swatch.svelte
@@ -0,0 +1,85 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+  import { randomHexColor } from './helpers';
+
+  let color = randomHexColor();
+  let animating = false;
+
+  function startAnimating(event: MouseEvent | TouchEvent | KeyboardEvent) {
+    if (animating) return;
+    const target = event.target as HTMLElement;
+    target.addEventListener('animationend', stopAnimating, { once: true });
+    animating = true;
+  }
+
+  function stopAnimating() {
+    animating = false;
+  }
+
+  function changeColor() {
+    if (animating) return;
+    color = randomHexColor();
+  }
+
+  let interval = Math.ceil(Math.random() * 26);
+
+  onMount(() => {
+    const timer = setInterval(changeColor, interval * 200);
+    return () => clearInterval(timer);
+  });
+</script>
+
+<div
+  class="swatch"
+  style="--color: {color}"
+  on:mouseenter={startAnimating}
+  on:click={startAnimating}
+  on:touchstart={startAnimating}
+  on:touchmove={startAnimating}
+  on:touchend={startAnimating}
+  on:keypress={startAnimating}
+  class:animating
+  role="button"
+  tabindex="0">
+</div>
+
+<style lang="less">
+  .swatch {
+    touch-action: none;
+    background-color: var(--color);
+    transition: background-color 2s ease-out;
+    border: 1px solid rgba(0 0 0 / 0.2);
+    border-radius: 4px;
+    aspect-ratio: 1;
+    &.animating {
+      pointer-events: none;
+      animation: bloom 2s ease-out forwards;
+    }
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-shadow:
+      1px 1px 1px rgba(0 0 0 / 1),
+      0px 0px 1px rgba(0 0 0 / 1);
+    font-family: var(--font-sans);
+    color: white;
+    font-weight: bold;
+    text-transform: uppercase;
+  }
+
+  @keyframes bloom {
+    0% {
+      scale: 1;
+      opacity: 1;
+    }
+    90% {
+      scale: 3;
+      opacity: 0.1;
+    }
+    100% {
+      scale: 1;
+      opacity: 1;
+    }
+  }
+</style>
---
+++ src/lib/deck/SwatchGrid.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import Swatch from './Swatch.svelte';
+
+  export let swatches = new Array(36).fill(null);
+</script>
+
+<div class="swatch-grid">
+  {#each swatches as _}
+    <Swatch />
+  {/each}
+</div>
+
+<style lang="less">
+  .swatch-grid {
+    width: 400px;
+    touch-action: none;
+    margin-block: 1rem;
+    display: grid;
+    gap: 4px;
+    grid-template-columns: repeat(6, 1fr);
+    grid-auto-rows: 1fr;
+    user-select: none;
+  }
+</style>
